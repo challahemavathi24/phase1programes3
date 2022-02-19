a2897561e328789fbed649e014522364ac83ae24
unknown <hemanaresh3024@gmail.com>
2022-02-19 15:15:34 +0530

    first commit on phase1-programs3

---
+++ src/com/innerclasses/CarImpl.java
@@ -0,0 +1,16 @@
+package com.innerclasses;
+
+	public class CarImpl implements Car{
+
+		public void start()
+		{
+			//logic for start
+			System.out.println("car Started");
+		}
+		public void stop()
+		{
+			//logic for stop
+			System.out.println("Car stopped");
+		}
+
+}
---
+++ src/com/innerclasses/UseCar.java
@@ -0,0 +1,17 @@
+package com.innerclasses;
+
+	public class UseCar {
+
+		public static void main(String[] args) {
+
+			Car car1=new CarImpl();
+			Car car2=new CarImpl();
+
+			car1.start();
+			car2.start();
+
+			car1.stop();
+			car2.stop();
+		}
+	}
+	
